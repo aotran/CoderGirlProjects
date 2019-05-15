a826ba8f9d24b24bcf13e5a4b1c59d4223e61b2c
Alex Eichhorn <aotran@gmail.com>
2019-05-15 18:59:23 -0500

    TapestryController - moved main into TapestryController class

--- TapestryController/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        new TapestryController(new CrossStitchWigglePattern());
-        new TapestryController(new ZigZagLinesPattern());
-        new TapestryController(new BaublePattern());
-        new TapestryController(new BlockyBaublePattern());
-        new TapestryController(new BrokenWigglePattern());
-    }
-}
--- TapestryController/src/TapestryController.java
+++ TapestryController/src/TapestryController.java
@@ -1,21 +1,33 @@
 public class TapestryController {
-    public TapestryController(Pattern pattern) {
-        for (int j = 0; j < 3; j++) {
-            for (int i = 0; i < 8; i++) {
-                pattern.pattern();
+
+    public TapestryController() { }
+
+    public static void main(String[] args) {
+
+        Pattern[] patternList = new Pattern[] {
+                new CrossStitchWigglePattern(),
+                new ZigZagLinesPattern(),
+                new BaublePattern(),
+                new BlockyBaublePattern(),
+                new BrokenWigglePattern()};
+        for (int k = 0; k < patternList.length; k++) {
+            for (int j = 0; j < 3; j++) {
+                for (int i = 0; i < 8; i++) {
+                    patternList[k].pattern();
+                }
+                System.out.println();
             }
-            System.out.println();
-        }
 
-        System.out.println("\n\n");
+            System.out.println("\n\n");
 
-        for (int j = 0; j < 8; j++) {
-            for (int i = 0; i < 6; i++) {
-                pattern.pattern();
+            for (int j = 0; j < 8; j++) {
+                for (int i = 0; i < 6; i++) {
+                    patternList[k].pattern();
+                }
+                System.out.println();
             }
-            System.out.println();
-        }
 
-        System.out.println("\n\n");
+            System.out.println("\n\n");
+        }
     }
 }
